2442124deced6f0ee1255db99a3320e8c42ad162
Dublin Anondson <stratophone2112@gmail.com>
2022-04-13 08:41:59 -0500

    feat: added sentry replay functionality for testing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import { SentryReplay } from '@sentry/replay';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing(),
+    new SentryReplay({
+    stickySession: true, // Default is false
+    rrwebConfig: {
+      maskAllInputs: false, // Default is true
+    },
+  }),],
+  autoSessionTracking :true,
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
